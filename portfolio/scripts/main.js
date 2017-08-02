a6f666e22f17b1870800217daceabbbd9d68a1a6
davidwhite478 <davidwhite478@gmail.com>
2017-08-02 02:50:47 +0100

    Updated Portfolio
    Updated Music Bingo, added Gitlab and Third Year Project

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -6,7 +6,7 @@ function showPreview(divId, backgroundDivId)
 
 function hidePreviews()
 {
-	for (var i = 0; i < arguments.length; i++) 
+	for (var i = 0; i < arguments.length; i++)
 	{
     	arguments[i].style.display='none';
     }
@@ -15,10 +15,10 @@ function hidePreviews()
 $(".contents .projectLink").click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).find("a").attr('href') ).offset().top-(document.body.offsetWidth*1/500)
-    }, 800);
+    }, 1200);
     return false;
 });
 
-$(".topArrow").click(function(){    
-    $("html, body").animate({ scrollTop: "0px" }, 800);
+$(".topArrow").click(function(){
+    $("html, body").animate({ scrollTop: "0px" }, 1200);
 });
